5876a61bd9bfc34458f6c2bfc8eda4ef61528c15
Steve <sthesengineer@gmail.com>
2019-12-21 13:42:34 +0400

    Atttempttt

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/async/translation/TranslationTask.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/async/translation/TranslationTask.java
@@ -232,13 +232,14 @@ public class TranslationTask extends AsyncTask {
                 int centreMinY = oldHitBox.getLocalMinY(midPoint.getX(), midPoint.getZ());
                 int groundY = centreMinY;
                 World w = craft.getW();
-                while (w.getBlockAt(midPoint.getX(), groundY - 1, midPoint.getZ()).getType() == Material.AIR || craft.getType().getPassthroughBlocks().contains(w.getBlockAt(midPoint.getX(), groundY -1, midPoint.getZ()).getType())){
+                while (w.getBlockAt(midPoint.getX(), groundY - 1, midPoint.getZ()).getType() == Material.AIR || craft.getType().getPassthroughBlocks().contains(w.getBlockAt(midPoint.getX(), groundY - 1, midPoint.getZ()).getType())){
                     groundY--;
+                    Bukkit.broadcastMessage("groundY--: " + groundY);
                 }
                 Bukkit.broadcastMessage("groundY: " + groundY);
                 Bukkit.broadcastMessage("centreMinY: " + centreMinY);
                 Bukkit.broadcastMessage("craft.getType().getHoverLimit(): " + craft.getType().getHoverLimit());
-                if (centreMinY - groundY - 1 > craft.getType().getHoverLimit()){
+                if (centreMinY - groundY > craft.getType().getHoverLimit()){
                     dy = -1;
                 }
             } else if (!isOnGround(oldHitBox)){
